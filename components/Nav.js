02f8a05cb0595c184038c025ed8a70da9bed7390
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-03-23 19:36:45 +0000

    fix /events/[eventId] (working)

--- components/Nav.js
+++ components/Nav.js
@@ -1,18 +1,34 @@
-import navStyles from '../styles/NavS.module.js';
+// import navStyles from '../styles/Nav.module.js';
 import Link from 'next/link';
 
 import React from 'react'
 
 const Nav = () => {
     return (
-        <nav className={navStyles.nav}>
+        <nav>
             <ul>
+                <li><Link href='/'>Home</Link></li>
                 <li>
-                    <Link href='/'>Home</Link>
+                    <ul>
+                        <li><Link href='/events'>Events</Link></li>
+                        <li><Link href='/events/add'>Add events</Link></li>
+                    </ul>  
                 </li>
                 <li>
-                    <Link href='/about'>About</Link>    
+                    <ul>
+                        <li><Link href='/users/login'>Login</Link></li>
+                        <li><Link href='/users/register'>Register</Link></li>
+                    </ul>
                 </li>
+                <li>
+                    <ul>
+                        <li><Link href='/store'>Store</Link></li>
+                        <li><Link href='/store/add'>Add to Store</Link></li>
+                    </ul>
+                </li>
+                <li><Link href='/study'>Study</Link></li>
+                <li><Link href='/members'>Numbers</Link></li>
+                <li><Link href='/about'>About</Link></li>
             </ul>
         </nav>
     )
--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -1,16 +1,14 @@
 const Event = ({ event }) => {
-    const router = useRouter()
-    const params = router.query;
     return (
         <div>
-            <h1>{console.log(event)}</h1>
+            <h1>{event.title}</h1>
         </div>
     )
 }
 
 export async function getStaticProps({ params }) {
     console.log(params);
-    const res = await fetch(`https://nordredo-backend.herokuapp.com/event/${params.id}`);
+    const res = await fetch(`https://nordredo-backend.herokuapp.com/event/${params.eventId}`);
     const event = await res.json();
     return {
         props: {
@@ -24,12 +22,12 @@ export async function getStaticPaths() {
     const events = await res.json();
     const ids = events.map((event) => event.id);
     const paths = ids.map((id) => ({ 
-        params: { id: id.toString() },
+        params: { eventId: id.toString() },
      }));
     console.log(paths);
     return {
         paths,
-        fallback: false
+        fallback: true,
     }
 }
 
