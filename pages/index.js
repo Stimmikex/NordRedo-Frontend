ab1620885e18b463a56f56e08b9387677e57297b
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-04-02 18:10:44 +0000

    integrated bookstore to in mainpage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import EventList from '../components/Events/EventList.js';
+import ItemList from '../components/Store/ItemList.js';
 
-export default function index({ events }) {
+
+export default function index({ events, items }) {
     return (
         <div>
+          <div>
             <h1>Upcoming Events</h1>
             <EventList events={events} get={"Active"}></EventList>
+          </div>
+          <div>
+            <h1>BookStore</h1>
+            <ItemList items={items}></ItemList>
+          </div>
         </div>
     )
 }
@@ -13,10 +21,13 @@ export default function index({ events }) {
 export async function getStaticProps() {
     const res = await fetch('https://nordredo-backend.herokuapp.com/')
     const events = await res.json()
+    const itemRes = await fetch('https://nordredo-backend.herokuapp.com/store')
+    const items = await itemRes.json()
     console.log(events);
     return {
       props: {
         events,
+        items,
       },
     }
   }
