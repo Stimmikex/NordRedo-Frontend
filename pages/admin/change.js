73d071df0a5b78597d3eeb99b2044b1f6ebd0d91
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-04-14 19:59:24 +0000

    added poppup to ads.js (temp)

--- pages/admin/change.js
+++ pages/admin/change.js
@@ -1,16 +1,43 @@
 import React from 'react'
+import Router from "next/router"
+import userPop from '../../styles/UserPopup.module.scss'
+import SearchUsers from '../../components/Users/SearchUsers.js';
 
-const change = ({ gov }) => {
+const change = ({ gov, users }) => {
+    const [isOpenChange, setIsOpenChange] = React.useState(false)
+
+    const ClosePopup = () => {
+        setIsOpenChange(false)
+      }
+
+    let OpenPopup = () => {
+        setIsOpenChange(true)
+        Router.push({ shallow: true })
+      }
+    
     return (
         <div>
             <h1>Change Government</h1>
             {gov.map((gover) => {
             return (
                 <div>
-                    <h2>{gover.username}</h2>
+                    <h2>{gover.title}</h2>
+                    <p>{gover.username}</p>
                     <p>Year: {gover.year}</p>
-                    <p>Title: {gover.title}</p>
-                    <button>Change</button>
+                    <button  onClick={e => OpenPopup()}>Change</button>
+                    {isOpenChange && (
+                    <div className={userPop.containerpop}>
+                        <div className={userPop.searchpop}>
+                            <div className={userPop.searchpop_header}>
+                                <p>Update user role for {gover.username}</p>
+                                <button onClick={ClosePopup}> X </button>
+                            </div>
+                            <div>
+                                <SearchUsers users={users}></SearchUsers>
+                            </div>
+                        </div>
+                    </div>
+                )}
                 </div>
             )
             })}
@@ -21,9 +48,12 @@ const change = ({ gov }) => {
 export async function getStaticProps() {
     const res = await fetch(`https://nordredo-backend.herokuapp.com/admin/gov`);
     const gov = await res.json();
+    const resUsers = await fetch(`https://nordredo-backend.herokuapp.com/admin/members`);
+    const users = await resUsers.json();
     return {
         props: {
             gov,
+            users,
         },
     }
 }
