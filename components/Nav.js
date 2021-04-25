caff0c405c9688de84ceb928d074eda132a315d7
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-04-25 17:41:55 +0000

    started styling the larger screen width header

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,6 @@
 import navStyles from '../styles/Nav.module.scss';
 import Link from 'next/link';
-import { faHome, faCalendarAlt, faCalendarCheck, faCalendarPlus, faStore, faPlusSquare, faUniversity, faUsers, faUser, faUserShield, faInfo, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faCalendarAlt, faCalendarCheck, faCalendarPlus, faStore, faPlusSquare, faUniversity, faUsers, faUser, faUserShield, faInfo, faUserPlus, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import React from 'react'
@@ -8,7 +8,6 @@ import React from 'react'
 const Nav = () => {
     function openNav() {
         const x = document.getElementById("mylinks");
-        console.log('testing click');
         if (x.style.display === "flex") {
             if(document.body.clientWidth > 600) {
                 x.style.display = "flex";
@@ -28,8 +27,33 @@ const Nav = () => {
             })
         }
     }
+    function openSubNav(id) {
+        const x = document.getElementById(id);
+        if (x.style.display === "flex") {
+            if(document.body.clientWidth < 600) {
+                x.style.display = "flex";
+            } else {
+                x.style.display = "none";
+            }
+        } else {
+          x.style.display = "flex";
+        }
+        if (typeof window !== "undefined") {
+            window.addEventListener("resize", function(event) {
+                if(document.body.clientWidth < 600) {
+                    x.style.display = "flex";
+                } else {
+                    x.style.display = "none";
+                }
+            })
+        }
+    }
+    function closeNav(id) {
+        openSubNav(id);
+        openNav();
+    }
     return (
-        <nav>
+        <nav className={navStyles.nav}>
             <div className={navStyles.nav_container_bar}>
                 <div className={navStyles.nav_container_bar_split} style={{background: `url('../../nordLogo/header.jpg')`}}>
                     <img src="../../nordLogo/nord-lit.svg"></img>
@@ -41,14 +65,20 @@ const Nav = () => {
                     <p onClick={openNav}><Link href='/'><div className={navStyles.linkContainer}><p>Home</p><i><FontAwesomeIcon icon={faHome} onClick={openNav} /></i></div></Link></p>
                 </div>
                 <div className={navStyles.nav_container_item}>
-                    <div className={navStyles.nav_container_item_event}>
-                        <p onClick={openNav}><Link href='/events'><div className={navStyles.linkContainer}><p>Events</p><i><FontAwesomeIcon icon={faCalendarCheck} /></i></div></Link></p>
-                        <p onClick={openNav}><Link href='/events/add'><div className={navStyles.linkContainer}><p>Add events</p><i><FontAwesomeIcon icon={faCalendarPlus} /></i></div></Link></p>
-                        <p onClick={openNav}><Link href='/events/calendar'><div className={navStyles.linkContainer}><p>Event calendar</p><i><FontAwesomeIcon icon={faCalendarAlt} /></i></div></Link></p>
+                    <div className={navStyles.nav_container_item_hidden}>
+                        <p onClick={e => closeNav('eventSub')}>Events<i><FontAwesomeIcon icon={faArrowDown}/></i></p>
                     </div>
-                    <div className={navStyles.nav_container_item_store}>
-                        <p onClick={openNav}><Link href='/store'><div className={navStyles.linkContainer}><p>Store</p><i><FontAwesomeIcon icon={faStore}/></i></div></Link></p>
-                        <p onClick={openNav}><Link href='/store/add'><div className={navStyles.linkContainer}><p>Add to Store</p><i><FontAwesomeIcon icon={faPlusSquare}/></i></div></Link></p>
+                    <div className={navStyles.nav_container_item_event} id='eventSub'>
+                        <p onClick={e => closeNav('eventSub')}><Link href='/events'><div className={navStyles.linkContainer}><p>Events</p><i><FontAwesomeIcon icon={faCalendarCheck} /></i></div></Link></p>
+                        <p onClick={e => closeNav('eventSub')}><Link href='/events/add'><div className={navStyles.linkContainer}><p>Add events</p><i><FontAwesomeIcon icon={faCalendarPlus} /></i></div></Link></p>
+                        <p onClick={e => closeNav('eventSub')}><Link href='/events/calendar'><div className={navStyles.linkContainer}><p>Event calendar</p><i><FontAwesomeIcon icon={faCalendarAlt} /></i></div></Link></p>
+                    </div>
+                    <div className={navStyles.nav_container_item_hidden}>
+                        <p onClick={e => closeNav('storeSub')}>Store<i><FontAwesomeIcon icon={faArrowDown}/></i></p>
+                    </div>
+                    <div className={navStyles.nav_container_item_store} id='storeSub'>
+                        <p onClick={e => closeNav('storeSub')}><Link href='/store'><div className={navStyles.linkContainer}><p>Store</p><i><FontAwesomeIcon icon={faStore}/></i></div></Link></p>
+                        <p onClick={e => closeNav('storeSub')}><Link href='/store/add'><div className={navStyles.linkContainer}><p>Add to Store</p><i><FontAwesomeIcon icon={faPlusSquare}/></i></div></Link></p>
                     </div>
                 </div>
                 <div className={navStyles.nav_container_about}>
