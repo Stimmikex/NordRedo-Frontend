7c39faeb00ebf8d943ff46fb83767e6ab609539d
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-05-07 16:54:57 +0000

    changing api url login

--- components/Users/LoginForm.js
+++ components/Users/LoginForm.js
@@ -4,7 +4,7 @@ import cookies from 'js-cookie';
 import Router from 'next/router'
 
 const {
-    REACT_APP_API_URL: apiUrl,
+    NEXT_PUBLIC_API_URL: apiUrl,
   } = process.env;
 
 const LoginForm = () => {
@@ -24,7 +24,7 @@ const LoginForm = () => {
             },
         }
 
-        const res = await fetch(`https://nordredo-backend.herokuapp.com/users/login`, options)
+        const res = await fetch(`${apiUrl}/users/login`, options)
         const result = await res.json()
         cookies.set('auth', result.token);
         Router.push('/events')
