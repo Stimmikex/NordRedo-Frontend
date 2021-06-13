4f50dff6420e8fa9c984103e171c03e1292f1624
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-06-13 19:23:19 +0000

    EventSearch by text added, just need to fix the backend

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,20 +1,29 @@
 import EventList from '../../components/Events/EventList.js';
 import { getUserWithCookie } from '../../components/fetchUserToken.js'
 import eventSearch from './EventSearch.module.scss'
+import React from 'react'
 
 const {
   NEXT_PUBLIC_API_URL: apiUrl,
 } = process.env;
 
 export default function Events({ events, user, types }) {
+  let [search, setSearch] = React.useState(events)
+
+  const handleChange = async (e) => {
+      const test = e.target.value;
+      const res = await fetch(`${apiUrl}/events/find/?name=${test}`);
+      const results = await res.json();
+      console.log(results)
+      setSearch(results);
+    }
   return (
       <div>
         <h1>Event List</h1>
         <div className={eventSearch.eventSearch}>
           <h1>Event Search</h1>
           <div className={eventSearch.eventSearch_text}>
-            <label>Search Name: </label>
-            <input type="text"></input>
+            <input type="text" placeholder="Search Name" onChange={handleChange}></input>
           </div>
           <div className={eventSearch.eventSearch_addons}>
             <div className={eventSearch.eventSearch_addons_active}>
